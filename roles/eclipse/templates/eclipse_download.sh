26a6be8bfe86655268242af21074293fd4e7d520
Sauli Kotisaari <skotisaari@localhost.ubisecure.ubi>
2015-05-01 22:29:38 +0300

    rdp added

---
+++ roles/eclipse/templates/eclipse_download.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo foo
+
+URL={{ eclipse_download_server }}
+#URL=download.eclipse.org/technology/epp/downloads/release
+VERSION=luna/SR2
+FILE=eclipse-jee-luna-SR2-linux-gtk-x86_64.tar.gz
+
+echo $URL/$VERSION/$FILE
+#wget $URL/luna/SR2/eclipse-jee-luna-SR2-linux-gtk-x86_64.tar.gz
+
+
+
